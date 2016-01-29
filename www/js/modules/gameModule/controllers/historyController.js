a85d215f231e63456a2336f5f29c3c3d52bf80e3
vista-54 <vista-54@ukr.net>
2016-01-29 03:51:49 +0200

    HostoryCtrlAdd

--- www/js/modules/gameModule/controllers/historyController.js
+++ www/js/modules/gameModule/controllers/historyController.js
@@ -15,11 +15,9 @@ function historyController($scope, $http, $rootScope) {
         $scope.Hgames = data.data;
 //            $scope.CurrGame = $rootScope.gameData.games[$rootScope.SearchGameById($scope.gameId)];
     });
-
     req.error(function (data, status, headers, config) {
         console.log(data);
     });
-
     $scope.checkHost = function (hostNick) {
         if (hostNick === $rootScope.userData.login) {
             return true;
@@ -32,23 +30,33 @@ function historyController($scope, $http, $rootScope) {
     {
         var ScoreHost = (parseInt(game.scoreP1r1) + (parseInt(game.scoreP1r2)) + parseInt(game.scoreP1r3) + parseInt(game.scoreP1r4) + parseInt(game.scoreP1r5) + parseInt(game.scoreP1r6));
         var ScorePlayer2 = (parseInt(game.scoreP2r1) + parseInt(game.scoreP2r2) + parseInt(game.scoreP2r3) + parseInt(game.scoreP2r4) + parseInt(game.scoreP2r5) + parseInt(game.scoreP2r6));
-        if (ScoreHost > ScorePlayer2 && $scope.checkHost(game.host)) {
-            return "Победа";
+        if ($scope.checkHost(game.host)) {
+            if (ScoreHost > ScorePlayer2) {
+                return "Победа";
+            }
+            else if (ScoreHost < ScorePlayer2)
+            {
+                return "Поражение";
+            }
+            else if (ScoreHost === ScorePlayer2) {
+                return "Ничья";
+            }
         }
-        else if (ScoreHost < ScorePlayer2 && $scope.checkHost(game.host))
-        {
-            return "Поражение";
+        else {
+            if (ScoreHost > ScorePlayer2) {
+                return "Поражение";
+            } else if (ScoreHost < ScorePlayer2 && !$scope.checkHost(game.host)) {
+                return "Победа";
+            }  else if (ScoreHost === ScorePlayer2) {
+                return "Ничья";
+            }
+
         }
-        else if (ScoreHost === ScorePlayer2) {
-            return "Ничья";
-        }
-        else if (ScoreHost > ScorePlayer2 && !$scope.checkHost(game.host)) {
-            return "Победа";
-        }
-        else if (ScoreHost < ScorePlayer2 && !$scope.checkHost(game.host))
-        {
-            return "Поражение";
-        }
+
+
+
+
+
 
         console.log("ScoreHost=>" + ScoreHost + "ScorePlayer2=>" + ScorePlayer2);
     };
