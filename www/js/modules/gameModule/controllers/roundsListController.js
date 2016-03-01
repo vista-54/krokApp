299e73cc9186ccd5b4682d64dae11007e04323d9
vista-54 <vista-54@ukr.net>
2016-03-02 01:36:14 +0200

    Добавлен модуль статистики(пока что визуальной) просмотра раундов.

--- www/js/modules/gameModule/controllers/roundsListController.js
+++ www/js/modules/gameModule/controllers/roundsListController.js
@@ -18,19 +18,26 @@ function roundsListController($scope, $rootScope, $routeParams, $http) {
         var req = $http.get($rootScope.mainUrl + "index.php?&action=getRoundStatistic&id_game=" + $scope.gameId);
         req.success(function (data, status, headers, config) {
             console.log(status, data);
-            for (var i in data.data) {
-                var obj = data.data[i];
-                if (obj.ishost === "true" && $rootScope.checkHost) {
-                    $scope.roundsP1 = obj;
-                    $rootScope.CurrentGame.lastAnsH = obj;
-                    $scope.Anyfnt();
-//                    $scope.ready = true;
-//                    $scope.$apply();
-                }
-                else {
-                    $scope.roundsP2 = obj;
-                }
-            }
+            $scope.Anyfnt();
+            $scope.rs = data.data[0];
+//            for (var i in $scope.rs) {
+//                console.log(i);
+//            }
+            console.log($scope.rs);
+            /* for (var i in data.data) {
+             var obj = data.data[i];
+             if ($rootScope.checkHost) {
+             $scope.host = obj;
+             //                    $rootScope.CurrentGame.lastAnsH = obj;
+             
+             //                    $scope.ready = true;
+             //                    $scope.$apply();
+             }
+             else {
+             $scope.p2 = obj;
+             }
+             
+             }*/
 //            console.log($scope.roundsP1 + " => " + $scope.roundsP2);
 
         });
@@ -39,104 +46,104 @@ function roundsListController($scope, $rootScope, $routeParams, $http) {
         });
     };
     $scope.getRoundStatistic();
-        /*Конец игры Бэта*/
-        $scope.checkHost = $rootScope.checkHost;
-        
-        $rootScope.gameEnd = function (game) {
-            var req = $http.get($rootScope.mainUrl + "index.php?&action=gameEnd&idgame=" + $scope.gameId);
-            req.success(function (data, status, headers, config) {
-                console.log(data);
-            });
-            req.error(function (data, status, headers, config) {
-                console.log(data);
-            });
-        };
-              $scope.isHost = function () {
-            if ($scope.currentGame.host === $rootScope.userData.login) {
-                $scope.currentRound = $rootScope.CurrentGame.round;
-                return true;
-            }
-            else {
-                $scope.currentRound = $rootScope.CurrentGame.round - 1;
-                return false;
-            }
-        };
+    /*Конец игры Бэта*/
+    $scope.checkHost = $rootScope.checkHost;
+    $scope.myNick = $rootScope.userData.login;
+    $rootScope.gameEnd = function (game) {
+        var req = $http.get($rootScope.mainUrl + "index.php?&action=gameEnd&idgame=" + $scope.gameId);
+        req.success(function (data, status, headers, config) {
+            console.log(data);
+        });
+        req.error(function (data, status, headers, config) {
+            console.log(data);
+        });
+    };
+    $scope.isHost = function () {
+        if ($scope.currentGame.host === $rootScope.userData.login) {
+            $scope.currentRound = $rootScope.CurrentGame.round;
+            return true;
+        }
+        else {
+            $scope.currentRound = $rootScope.CurrentGame.round - 1;
+            return false;
+        }
+    };
 //    $scope.isHost();
-        console.log("roundsListController");
-        $scope.getCurrentCategory = function (catId) {
-            var req = $http.get($rootScope.mainUrl + "index.php?&action=getCurrCat&idcat=" + catId + "&lng=" + $rootScope.userData.lng);
-            req.success(function (data, status, headers, config) {
-                console.log(data);
-                $rootScope.CurrentGame.categoryName = data.data[0];
-            });
-            req.error(function (data, status, headers, config) {
-                console.log(data);
-            });
-        };
-        $scope.play = function () {
-            $rootScope.CurrentGame.isCategotySelected = false;
-            //проверяем раунд 
-            switch ($rootScope.getCurrentRound($scope.game)) {
-                case 1:
-                    res = $scope.game.round1;
-                    break;
-                case 2:
-                    res = $scope.game.round2;
-                    break;
-                case 3:
-                    res = $scope.game.round3;
-                    break;
-                case 4:
-                    res = $scope.game.round4;
-                    break;
-                case 5:
-                    res = $scope.game.round5;
-                    break;
-                case 6:
-                    res = $scope.game.round6;
-                    break;
+    console.log("roundsListController");
+    $scope.getCurrentCategory = function (catId) {
+        var req = $http.get($rootScope.mainUrl + "index.php?&action=getCurrCat&idcat=" + catId + "&lng=" + $rootScope.userData.lng);
+        req.success(function (data, status, headers, config) {
+            console.log(data);
+            $rootScope.CurrentGame.categoryName = data.data[0];
+        });
+        req.error(function (data, status, headers, config) {
+            console.log(data);
+        });
+    };
+    $scope.play = function () {
+        $rootScope.CurrentGame.isCategotySelected = false;
+        //проверяем раунд 
+        switch ($rootScope.getCurrentRound($scope.game)) {
+            case 1:
+                res = $scope.game.round1;
+                break;
+            case 2:
+                res = $scope.game.round2;
+                break;
+            case 3:
+                res = $scope.game.round3;
+                break;
+            case 4:
+                res = $scope.game.round4;
+                break;
+            case 5:
+                res = $scope.game.round5;
+                break;
+            case 6:
+                res = $scope.game.round6;
+                break;
 
-            }
-            //берем с раунда катеорию если она не НУЛЛ
-            if (res !== null) {
-                $scope.getCurrentCategory(res);
-                $rootScope.CurrentGame.category = {
-                    id: res
-                };
-                $rootScope.CurrentGame.isCategotySelected = true;
-                window.location = "#/choisecategoty";
-            }
-            //Если нул получаем список с сервера
-            else {
-                $scope.getCategoriesList();
-            }
+        }
+        //берем с раунда катеорию если она не НУЛЛ
+        if (res !== null) {
+            $scope.getCurrentCategory(res);
+            $rootScope.CurrentGame.category = {
+                id: res
+            };
+            $rootScope.CurrentGame.isCategotySelected = true;
+            window.location = "#/choisecategoty";
+        }
+        //Если нул получаем список с сервера
+        else {
+            $scope.getCategoriesList();
+        }
 
-            $rootScope.CurrentGame.id = $scope.gameId;
+        $rootScope.CurrentGame.id = $scope.gameId;
 
 
-        };
-        $scope.getCategoriesList = function () {
-            var req = $http.get($rootScope.mainUrl + "index.php?&action=getCategory&lng=" + $rootScope.userData.lng);
-            req.success(function (data, status, headers, config) {
+    };
+    $scope.getCategoriesList = function () {
+        var req = $http.get($rootScope.mainUrl + "index.php?&action=getCategory&lng=" + $rootScope.userData.lng);
+        req.success(function (data, status, headers, config) {
 //            console.log(status, data);
-                $rootScope.gameData.games[$scope.SearchGameById($scope.gameId)].EmptyCategoriesList = data.data;
-                $rootScope.gameData.games[$scope.SearchGameById($scope.gameId)].BusyCategoriesList = [];
-                window.location = "#/choisecategoty";
-            });
-            req.error(function (data, status, headers, config) {
-                console.log(data);
-            });
-        };
+            $rootScope.gameData.games[$scope.SearchGameById($scope.gameId)].EmptyCategoriesList = data.data;
+            $rootScope.gameData.games[$scope.SearchGameById($scope.gameId)].BusyCategoriesList = [];
+            window.location = "#/choisecategoty";
+        });
+        req.error(function (data, status, headers, config) {
+            console.log(data);
+        });
+    };
 
     $scope.Anyfnt = function () {
-        
+
 //        $scope.ready = false;
-        
+
 //    $scope.PlayButton='';
 //    $scope.waitStep=' '
 
 
-    
+
 //    $scope.currentRound=1;
         /*
          $scope.getOpenGames = function () {
@@ -179,37 +186,128 @@ function roundsListController($scope, $rootScope, $routeParams, $http) {
 //        }
 //    };
 //    $scope.currentRound = $rootScope.getCurrentRound();
-  
+
 
 
 
         $scope.getRoundStat = function (Round_num, p) {
-            var count = 0;
-            if (p === 'host') {
-                var g = JSON.parse($scope.roundsP1.game);
-                var game = g.ans;
-                for (var i in game) {
-                    var obj = game[i];
-                    if (obj.user_ans_status === "1") {
-                        count++;
+            var answeresUser, dn;
+            if (p) {
+                switch (Round_num) {
+                    case 1:
+                        answeresUser = $scope.rs.r1p1;
+                        dn = 'r1p1';
+                        break;
+                    case 2:
+                        answeresUser = $scope.rs.r2p1;
+                        dn = 'r2p1';
+                        break;
+                    case 3:
+                        answeresUser = $scope.rs.r3p1;
+                        dn = 'r3p1';
+                        break;
+                    case 4:
+                        answeresUser = $scope.rs.r4p1;
+                        dn = 'r4p1';
+                        break;
+                    case 5:
+                        answeresUser = $scope.rs.r5p1;
+                        dn = 'r5p1';
+                        break;
+                    case 6:
+                        answeresUser = $scope.rs.r6p1;
+                        dn = 'r6p1';
+                        break;
+                }
+                $scope.CurrAnswUser = JSON.parse(answeresUser);
+                var images = angular.element(document.querySelectorAll('[data-round]'));
+
+                for (var i in images) {
+
+                    var obj = images[i];
+                    var chArr = [];
+                    for (var k = 0; k < 6; k++) {
+                        if (obj.children[k].localName !== "br") {
+                            chArr.push(obj.children[k]);
+                        }
+
                     }
 
-                }
 
-//          console.log(count);
+                    if (obj.attributes['data-round'].nodeValue === dn)
+                    {
+
+                        for (var j in $scope.CurrAnswUser) {
+                            var obj1 = $scope.CurrAnswUser[j];
+                            if (obj1.user_ans_status === '0') {
+//                                if (obj.children[j].localName !== "br") {
+                                $(chArr[j]).attr('src', 'img/krestik.png');
+//                                }
+                            }
+                        }
+                    }
+                }
             }
             else {
-                var g = JSON.parse($scope.roundsP2.game);
-                var game = g.ans;
-                for (var i in game) {
-                    var obj = game[i];
-                    if (obj.user_ans_status === "1") {
-                        count++;
+                switch (Round_num) {
+                    case 1:
+                        answeresUser = $scope.rs.r1p2;
+                        dn = 'r1p2';
+                        break;
+                    case 2:
+                        answeresUser = $scope.rs.r2p2;
+                        dn = 'r2p2';
+                        break;
+                    case 3:
+                        answeresUser = $scope.rs.r3p2;
+                        dn = 'r3p2';
+                        break;
+                    case 4:
+                        answeresUser = $scope.rs.r4p2;
+                        dn = 'r4p2';
+                        break;
+                    case 5:
+                        answeresUser = $scope.rs.r5p2;
+                        dn = 'r5p2';
+                        break;
+                    case 6:
+                        answeresUser = $scope.rs.r6p2;
+                        dn = 'r6p2';
+                        break;
+                }
+                $scope.CurrAnswUser = JSON.parse(answeresUser);
+                var images = angular.element(document.querySelectorAll('[data-round]'));
+
+                for (var i in images) {
+
+                    var obj = images[i];
+                    var chArr = [];
+                    for (var k = 0; k < 6; k++) {
+                        if (obj.children[k].localName !== "br") {
+                            chArr.push(obj.children[k]);
+                        }
+
                     }
 
+
+                    if (obj.attributes['data-round'].nodeValue === dn)
+                    {
+
+                        for (var j in $scope.CurrAnswUser) {
+                            var obj1 = $scope.CurrAnswUser[j];
+                            if (obj1.user_ans_status === '0') {
+//                                if (obj.children[j].localName !== "br") {
+                                $(chArr[j]).attr('src', 'img/krestik.png');
+//                                }
+                            }
+                        }
+                    }
                 }
             }
-            return count;
+
+
+
         };
-    };
+    }
+    ;
 }
